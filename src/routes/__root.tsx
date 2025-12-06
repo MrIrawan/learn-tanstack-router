10bc64802cdc4571ac0109b82abf22927fceee38
farrel irawan <irawanssfarrel@gmail.com>
2025-12-06 12:08:44 +0700

    adding site header component into __root file and adding root UI for __root file

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,18 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import SiteHeader from "../components/SiteHeader/site-header";
 
 const RootLayout = () => (
   <>
-    <Outlet />
-    <TanStackRouterDevtools />
+    <div className="min-h-screen bg-gray-50">
+      <SiteHeader />
+
+      <main className="max-w-7xl mx-auto p-6">
+        <Outlet />
+      </main>
+
+      <TanStackRouterDevtools position="bottom-right" />
+    </div>
   </>
 );
 
