8a5ad52cba282f4a37c086fe5313bed8b7f5b0e3
farrel irawan <irawanssfarrel@gmail.com>
2025-12-06 12:03:38 +0700

    adding site header component

---
+++ src/components/SiteHeader/site-header.tsx
@@ -0,0 +1,38 @@
+import { Link } from "@tanstack/react-router";
+
+export default function SiteHeader() {
+  return (
+    <nav className="bg-gradient-to-r from-blue-600 to-purple-600 text-white shadow-xl">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex gap-10 text-lg font-semibold">
+        <Link
+          to="/"
+          className="hover:opacity-80 transition"
+          activeProps={{ className: "underline underline-offset-4 font-bold" }}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="hover:opacity-80 transition"
+          activeProps={{ className: "underline underline-offset-4 font-bold" }}
+        >
+          About
+        </Link>
+        <Link
+          to="/dashboard"
+          className="hover:opacity-80 transition"
+          activeProps={{ className: "underline underline-offset-4 font-bold" }}
+        >
+          Dashboard
+        </Link>
+        <Link
+          to="/login"
+          className="hover:opacity-80 transition"
+          activeProps={{ className: "underline underline-offset-4 font-bold" }}
+        >
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+}
