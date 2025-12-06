8de76abfae01150177658d742a6d1da141a4f805
farrel irawan <irawanssfarrel@gmail.com>
2025-12-06 12:45:22 +0700

    implement UI for dashboard root route page

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/dashboard/')({
+export const Route = createFileRoute("/dashboard/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/"!</div>
+  return (
+    <div className="bg-white rounded-2xl shadow-xl p-10">
+      <h1 className="text-5xl font-bold mb-6">Welcome back, Farrel!</h1>
+      <p className="text-2xl text-gray-600">
+        Ini dashboard utama. Sidebar kiri udah aktif!
+      </p>
+    </div>
+  );
 }
